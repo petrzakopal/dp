16161924d2c58ed554b301cdbf83ba1b5fc0e856
petrzakopal <petr.zakopal@gmail.com>
2023-02-07 11:49:49 +0100

    test adderu z examplu, asi nebude fungovat

--- code/test-program/testing-data-moving/krnl_vadd.cpp
+++ code/test-program/testing-data-moving/krnl_vadd.cpp
@@ -1,177 +1,137 @@
-/*******************************************************************************
-Vendor: Xilinx
-Associated Filename: vadd.h
-Purpose: VITIS vector addition
-
-*******************************************************************************
-Copyright (C) 2019 XILINX, Inc.
-
-This file contains confidential and proprietary information of Xilinx, Inc. and
-is protected under U.S. and international copyright and other intellectual
-property laws.
-
-DISCLAIMER
-This disclaimer is not a license and does not grant any rights to the materials
-distributed herewith. Except as otherwise provided in a valid license issued to
-you by Xilinx, and to the maximum extent permitted by applicable law:
-(1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX
-HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
-INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
-FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
-in contract or tort, including negligence, or under any other theory of
-liability) for any loss or damage of any kind or nature related to, arising under
-or in connection with these materials, including for any direct, or any indirect,
-special, incidental, or consequential loss or damage (including loss of data,
-profits, goodwill, or any type of loss or damage suffered as a result of any
-action brought by a third party) even if such damage or loss was reasonably
-foreseeable or Xilinx had been advised of the possibility of the same.
-
-CRITICAL APPLICATIONS
-Xilinx products are not designed or intended to be fail-safe, or for use in any
-application requiring fail-safe performance, such as life-support or safety
-devices or systems, Class III medical devices, nuclear facilities, applications
-related to the deployment of airbags, or any other applications that could lead
-to death, personal injury, or severe property or environmental damage
-(individually and collectively, "Critical Applications"). Customer assumes the
-sole risk and liability of any use of Xilinx products in Critical Applications,
-subject only to applicable laws and regulations governing limitations on product
-liability.
-
-THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
-ALL TIMES.
-
-*******************************************************************************/
+/**
+* Copyright (C) 2019-2021 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 
 /*******************************************************************************
 Description:
+    HLS Dataflow Example using HLS Stream datatype
+    This is example of vector addition to demonstrate HLS Dataflow Pragma
+    functionality to perform task level parallelism using HLS Stream datatype.
+HLS
+    dataflow pragma instruct compiler to run sub-task parallel. Sub-task can
+    transfer the data using hls stream. In this Example, a vector addition
+    implementation is divided into three sub-task APIs as below:
 
-    This example uses the load/compute/store coding style which is generally
-    the most efficient for implementing kernels using HLS. The load and store
-    functions are responsible for moving data in and out of the kernel as
-    efficiently as possible. The core functionality is decomposed across one
-    of more compute functions. Whenever possible, the compute function should
-    pass data through HLS streams and should contain a single set of nested loops.
+    1) read_input():
+        This API reads the input vector from Global Memory and writes it into
+        HLS Stream 'inStream' using blocking write command.
 
-    HLS stream objects are used to pass data between producer and consumer
-    functions. Stream read and write operations have a blocking behavior which
-    allows consumers and producers to synchronize with each other automatically.
+    2) compute_add():
+        This API reads the input vector from 'inStream' using blocking read
+        command and increment the value by user specified increment. It writes
+        the results into 'outStream' using blocking write command.
 
-    The dataflow pragma instructs the compiler to enable task-level pipelining.
-    This is required for to load/compute/store functions to execute in a parallel
-    and pipelined manner. Here the kernel loads, computes and stores NUM_WORDS integer values per
-    clock cycle and is implemented as below:
-                                       _____________
-                                      |             |<----- Input Vector 1 from Global Memory
-                                      |  load_input |       __
-                                      |_____________|----->|  |
-                                       _____________       |  | in1_stream
-Input Vector 2 from Global Memory --->|             |      |__|
-                               __     |  load_input |        |
-                              |  |<---|_____________|        |
-                   in2_stream |  |     _____________         |
-                              |__|--->|             |<--------
-                                      | compute_add |      __
-                                      |_____________|---->|  |
-                                       ______________     |  | out_stream
-                                      |              |<---|__|
-                                      | store_result |
-                                      |______________|-----> Output result to Global Memory
+    3) write_result():
+        This API reads the result vector from 'outStream' using blocking read
+        command and write the result into Global Memory Location.
+
+    Data Flow Stream based Adder will be implemented as below:
+                    _____________
+                    |             |<----- Input Vector from Global Memory
+                    |  read_input |       __
+                    |_____________|----->|  |
+                     _____________       |  | inStream
+                    |             |<-----|__|
+                    | compute_add |       __
+                    |_____________|----->|  |
+                     _____________       |  | outStream
+                    |              |<----|__|
+                    | write_result |
+                    |______________|-----> Output result to Global Memory
+
 
 *******************************************************************************/
 
 // Includes
-#include <stdint.h>
+#include <ap_int.h>
 #include <hls_stream.h>
-#include <math.h>
 
 #define DATA_SIZE 4096
 
 // TRIPCOUNT identifier
 const int c_size = DATA_SIZE;
 
-static void load_input(float* in, hls::stream<float>& inStream) {
+// Read Data from Global Memory and write into Stream inStream
+static void read_input(unsigned int* in, hls::stream<unsigned int>& inStream, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
 mem_rd:
-    for (int i = 0; i < 1 ; i++) {
+    for (int i = 0; i < size; i++) {
 #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking write command to inStream
         inStream << in[i];
     }
 }
 
-
-static float dydx(float x, float y)
-{
-	#pragma HLS INLINE
-    return((x-y)/2);
-}
-
-static void rungeKutta(hls::stream<float>& in1_stream,
-                        hls::stream<float>& in2_stream,
-                        hls::stream<float>& out_stream) {
-
-
-    float x = 2, h = 0.00002;
-    float x0 = in1_stream.read();
-    float y = in2_stream.read();
-    float y0 = y;
-    // Number of iterations
-    int n = (int)((x-x0)/h);
-    float k1, k2, k3, k4;
-
-
-
-    // Iterating for number of iterations
+// Read Input data from inStream and write the result into outStream
+static void compute_add(hls::stream<unsigned int>& inStream, hls::stream<unsigned int>& outStream, int inc, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
 execute:
-    for(int i = 0; i<=n;i++)
-    {
-        #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-        k1 = h*dydx(x0,y0);
-        k2 = h*dydx(x0 + 0.5*h, y + 0.5*k1);
-        k3 = h*dydx(x0 + 0.5*h, y + 0.5*k2);
-        k4 = h*dydx(x0 + h, y + k3);
-
-        // Updating the y and x values for new iteration
-        y = y + (1.0/6.0) * (k1 + 2*k2 + 2*k3 + k4);
-        x0 = x0 + h;
-    }
-
-    out_stream << y;
-}
-
-static void compute(float number1, float number2)
-{
-	 	float x0 = number1;
-	    float y = number2;
-
-	    out_stream << (x0+y);
-
-}
-
-static void store_result(float* out, hls::stream<float>& out_stream) {
-mem_wr:
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < size; i++) {
 #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-        out[i] = out_stream.read();
+        // Blocking read command from inStream and Blocking write command
+        // to outStream
+        outStream << (inStream.read() + inc);
+    }
+}
+
+// Read result from outStream and write the result to Global Memory
+static void write_result(unsigned int* out, hls::stream<unsigned int>& outStream, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+mem_wr:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking read command to inStream
+        out[i] = outStream.read();
     }
 }
 
 extern "C" {
-
-
-void krnl_vadd(float in1, float in2, float* out) {
-// #pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
-// #pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
-#pragma HLS INTERFACE m_axi port = out bundle = gmem0
-
-    // static hls::stream<float> in1_stream("input_stream_1");
-    // static hls::stream<float> in2_stream("input_stream_2");
-    static hls::stream<float> out_stream("output_stream");
+/*
+    Vector Addition Kernel Implementation using dataflow
+    Arguments:
+        in   (input)  --> Input Vector
+        out  (output) --> Output Vector
+        inc  (input)  --> Increment
+        size (input)  --> Size of Vector in Integer
+   */
+void adder(unsigned int* in, unsigned int* out, int inc, int size) {
+    // Adding names for the streams. It allows the name to be used in reporting.
+    // Vivado HLS
+    // automatically checks to ensure all elements from an input stream are read
+    // during sw emulation.
+    static hls::stream<unsigned int> inStream("input_stream");
+    static hls::stream<unsigned int> outStream("output_stream");
+#pragma HLS STREAM variable = inStream depth = 32
+#pragma HLS STREAM variable = outStream depth = 32
+//  HLS STREAM variable=<name> depth=<size> pragma is used to define the Stream
+//  depth. For this example, Depth 32 is defined. Which means that Stream can
+//  hold
+//  maximum 32 outstanding elements at a given time. If Stream is full, any
+//  further
+//  blocking write command from producer will go into wait state until consumer
+//  reads some elements from stream. Similarly if Stream is empty (no element in
+//  Stream)
+//  any blocking read command from consumer will go into wait state until
+//  producer
+//  writes elements to Stream. This blocking read and write allow consumer and
+//  producer to synchronize each other.
 
 #pragma HLS dataflow
     // dataflow pragma instruct compiler to run following three APIs in parallel
-    // load_input(in1, in1_stream);
-    // load_input(in2, in2_stream);
-   // rungeKutta(in1_stream, in2_stream, out_stream);
-    compute(in1, in2, out_stream);
-    store_result(out, out_stream);
+    read_input(in, inStream, size);
+    compute_add(inStream, outStream, inc, size);
+    write_result(out, outStream, size);
 }
 }
