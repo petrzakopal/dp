996d5f84040b2729b1e1f6cb5a2ab0cd96cc3fc2
petrzakopal <petr.zakopal@gmail.com>
2023-02-08 03:06:48 +0100

    přidány poznámky ze 07.02.2023

--- code/test-program/testing-data-moving/krnl_vadd.cpp
+++ code/test-program/testing-data-moving/krnl_vadd.cpp
@@ -1,46 +1,18 @@
-/*******************************************************************************
-Vendor: Xilinx
-Associated Filename: vadd.h
-Purpose: VITIS vector addition
-
-*******************************************************************************
-Copyright (C) 2019 XILINX, Inc.
-
-This file contains confidential and proprietary information of Xilinx, Inc. and
-is protected under U.S. and international copyright and other intellectual
-property laws.
-
-DISCLAIMER
-This disclaimer is not a license and does not grant any rights to the materials
-distributed herewith. Except as otherwise provided in a valid license issued to
-you by Xilinx, and to the maximum extent permitted by applicable law:
-(1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX
-HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
-INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
-FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
-in contract or tort, including negligence, or under any other theory of
-liability) for any loss or damage of any kind or nature related to, arising under
-or in connection with these materials, including for any direct, or any indirect,
-special, incidental, or consequential loss or damage (including loss of data,
-profits, goodwill, or any type of loss or damage suffered as a result of any
-action brought by a third party) even if such damage or loss was reasonably
-foreseeable or Xilinx had been advised of the possibility of the same.
-
-CRITICAL APPLICATIONS
-Xilinx products are not designed or intended to be fail-safe, or for use in any
-application requiring fail-safe performance, such as life-support or safety
-devices or systems, Class III medical devices, nuclear facilities, applications
-related to the deployment of airbags, or any other applications that could lead
-to death, personal injury, or severe property or environmental damage
-(individually and collectively, "Critical Applications"). Customer assumes the
-sole risk and liability of any use of Xilinx products in Critical Applications,
-subject only to applicable laws and regulations governing limitations on product
-liability.
-
-THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
-ALL TIMES.
-
-*******************************************************************************/
+/**
+* Copyright (C) 2019-2021 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 
 /*******************************************************************************
 Description:
@@ -79,36 +51,118 @@ Input Vector 2 from Global Memory --->|             |      |__|
 
 *******************************************************************************/
 
+
+// All of these functions are ACCELERATED on the FPGA device (in PL)
+
 // Includes
-#include <stdint.h>
+// Declares sets of integer types having a specified widths
+// https://pubs.opengroup.org/onlinepubs/009695399/basedefs/stdint.h.html
+#include <stdint.h> 
+
+// Library for streams - optimization of code - paralelism
+// More about streams https://docs.xilinx.com/r/en-US/ug1399-vitis-hls/Using-HLS-Streams
 #include <hls_stream.h>
-#include <math.h>
 
 #define DATA_SIZE 4096
 
 // TRIPCOUNT identifier
+// The tripcount shows the total number of loop iterations.
+// More about tripcount, or loop iterations https://docs.xilinx.com/r/en-US/ug1399-vitis-hls/pragma-HLS-loop_tripcount
+// Probably defines number of iterations/tripcounts of loop
 const int c_size = DATA_SIZE;
 
 
+/*******************************************************************************/
+// Function for loading inputs from a Global Memory
+// Second argument is stream (not RAM but FIFO memory - sequential) When streams are passed into and out of functions, they must be passed-by-reference (eg. &)
 
-static void compute_test(int data, float* out)
-{
+static void load_input(uint32_t* in, hls::stream<uint32_t>& inStream, int size) {
+mem_rd:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // here << operator does not mean bit shifting but „Insertion Operators“
+        // more about Insertion operators at https://faculty.cs.niu.edu/~hutchins/csci241/io-op.htm
+        // In xilinx it is called blocking API at https://docs.xilinx.com/r/en-US/ug1399-vitis-hls/Blocking-API
+        inStream << in[i];
 
-	    out[0] = data;
-
+    }
 }
+/*******************************************************************************/
 
+
+
+/*******************************************************************************/
+// Function 
+static void compute_add(hls::stream<uint32_t>& in1_stream,
+                        hls::stream<uint32_t>& in2_stream,
+                        hls::stream<uint32_t>& out_stream,
+                        int size) {
+// The kernel is operating with vector of NUM_WORDS integers. The + operator performs
+// an element-wise add, resulting in NUM_WORDS parallel additions.
+execute:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+
+        // Read Blocking Read methods READS from the head of the stream and assigns the values to destination variable my_stream.read(dst_var); (not here)
+        // Here it reads the data from stream, sums it up and then inserts it in the out_stream stream
+        out_stream << (in1_stream.read() + in2_stream.read());
+    }
+}
+/*******************************************************************************/
+
+
+/*******************************************************************************/
+static void store_result(uint32_t* out, hls::stream<uint32_t>& out_stream, int size) {
+mem_wr:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        out[i] = out_stream.read();
+    }
+}
+/*******************************************************************************/
+
+
+// Start of the Kernel block
+// Vitis requires C++ kernels to be declared as extern “C” to avoid name mangling issues
 extern "C" {
 
+/*
+    Vector Addition Kernel
 
-void krnl_vadd(float* in1, float* in2, float* out, int size) {
+    Arguments:
+        in1  (input)  --> Input vector 1
+        in2  (input)  --> Input vector 2
+        out  (output) --> Output vector
+        size (input)  --> Number of elements in vector
+*/
+
+
+// Basically a main function for kernel to be executed/implemented in PL
+void krnl_vadd(uint32_t* in1, uint32_t* in2, uint32_t* out, int size) {
+// more about #pragma HLS INTERFACE https://docs.xilinx.com/r/en-US/ug1399-vitis-hls/pragma-HLS-interface
+    // mode=<mode> eg. m_axi – Implements all ports as an AXI4 interface.
+    // port=<name> – Specifies the name of the function argument or function return which the INTERFACE pragma applies to.
+    // bundle=<string> – By default, the HLS tool groups or bundles function arguments with compatible options into interface ports in the RTL code. Similarly, all function arguments specified as an AXI4 (m_axi) interface are bundled into a single AXI4 port by default. All interface ports with compatible options, such as mode, offset, and bundle, are grouped into a single interface port. The port name is derived automatically from a combination of the mode and bundle, or is named as specified by -name.
 #pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
 #pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
 #pragma HLS INTERFACE m_axi port = out bundle = gmem0
 
+    // defining stream which corresponds to name in the parenthesis because of error logging, otherwise it would show only eg.
+    // hls::stream<uint8_t> bytestr_in1;
+    // hls::stream<uint8_t> bytestr_in2("input_stream2");
+    // WHEN ERROR
+    // WARNING: Hls::stream 'hls::stream<unsigned char>.1' contains leftover data, which may result in RTL simulation hanging.
+    // WARNING: Hls::stream 'input_stream2' contains leftover data, which may result in RTL simulation hanging.
+    static hls::stream<uint32_t> in1_stream("input_stream_1");
+    static hls::stream<uint32_t> in2_stream("input_stream_2");
+    static hls::stream<uint32_t> out_stream("output_stream");
 
 #pragma HLS dataflow
-    compute_test(size, out);
-
+    // dataflow pragma instruct compiler to run following three APIs in parallel
+    // not definetely APIs but functions declared and defined up it the code
+    load_input(in1, in1_stream, size);
+    load_input(in2, in2_stream, size);
+    compute_add(in1_stream, in2_stream, out_stream, size);
+    store_result(out, out_stream, size);
 }
 }
--- code/test-program/testing-data-moving/vadd.cpp
+++ code/test-program/testing-data-moving/vadd.cpp
@@ -1,12 +1,20 @@
-/*******************************************************************************
-Vendor: Petr Zakopal, CTU FEE
-Associated Filename: vadd.cpp
-Purpose: Test of RK4
+/**
+* Copyright (C) 2019-2021 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 
-*******************************************************************************
-
-*******************************************************************************/
-
+// Function used to check if the function went well
 #define OCL_CHECK(error, call)                                                                   \
     call;                                                                                        \
     if (error != CL_SUCCESS) {                                                                   \
@@ -14,18 +22,24 @@ Purpose: Test of RK4
         exit(EXIT_FAILURE);                                                                      \
     }
 
+// Include libraries
 #include "vadd.h"
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
-#include <cmath>
 
+// Defining data size same as in kernel
+// It is the size of the vector - in position - not in sizeof(variable_type)
 static const int DATA_SIZE = 4096;
 
+// Consntant static string of error message
 static const std::string error_message =
     "Error: Result mismatch:\n"
     "i = %d CPU result = %d Device result = %d\n";
 
+// argc = argument count
+// argv = argument vector
+// basically argc is number of arguments +1, because most programs append number of programm to the arguments vector, but not sure if this is the case
 int main(int argc, char* argv[]) {
     // TARGET_DEVICE macro needs to be passed from gcc command line
     if (argc != 2) {
@@ -33,16 +47,19 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
+    // Getting the name of the binary file which is passed as an argument in vector
     std::string xclbinFilename = argv[1];
 
     // Compute the size of array in bytes
+    // sizeof(<data_type>) in ARM processors: https://developer.arm.com/documentation/dui0472/k/C-and-C---Implementation-Details/Basic-data-types-in-ARM-C-and-C--
     size_t size_in_bytes = DATA_SIZE * sizeof(int);
 
     // Creates a vector of DATA_SIZE elements with an initial value of 10 and 32
     // using customized allocator for getting buffer alignment to 4k boundary
 
     std::vector<cl::Device> devices;
-    cl_int err;
+    cl::Device device;
+    cl_int err; // cl_int is defined type for OpenCL devices - has ALWAYS fixed size of 32 bits
     cl::Context context;
     cl::CommandQueue q;
     cl::Kernel krnl_vector_add;
@@ -52,96 +69,122 @@ int main(int argc, char* argv[]) {
 
     // traversing all Platforms To find Xilinx Platform and targeted
     // Device in Xilinx Platform
-    cl::Platform::get(&platforms);
+    cl::Platform::get(&platforms); // Gets list of available platforms and pastes the address of the firts one to variable platforms, which is an arrray, better std::vector
     for (size_t i = 0; (i < platforms.size()) & (found_device == false); i++) {
         cl::Platform platform = platforms[i];
         std::string platformName = platform.getInfo<CL_PLATFORM_NAME>();
+
+        // If found the platform name Xilinx
         if (platformName == "Xilinx") {
+            // Be sure thah devices vector is empty
             devices.clear();
+            // Gets list of devices for found Xilinx platform TYPE ACCELERATOR and pastes the device in devices vector
             platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices);
+
+            // If there is some device found that matches the requirements (Xilinx and Accelerator)
+            // Put the device specs to cl::Device variable
             if (devices.size()) {
+                device = devices[0];
                 found_device = true;
                 break;
             }
         }
     }
     if (found_device == false) {
-        std::cout << "Error: Unable to find Target Device " << std::endl;
+        std::cout << "Error: Unable to find Target Device " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
         return EXIT_FAILURE;
     }
 
+    // Creating Context and Command Queue for selected device
+    OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
+
+    // CL_QUEUE_PROFILING_ENABLE - profiling objects are enabled by this parameter passed to parameter properties of CommandQueue
+    // Event objects can be used to capture profiling information that measure execution time of a command. 
+    OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+
     std::cout << "INFO: Reading " << xclbinFilename << std::endl;
     FILE* fp;
+
+    // c_str Returns a pointer to an array that contains a null-terminated sequence of characters (i.e., a C-string) representing the current value of the string object.
+    // opening file
     if ((fp = fopen(xclbinFilename.c_str(), "r")) == nullptr) {
         printf("ERROR: %s xclbin not available please build\n", xclbinFilename.c_str());
         exit(EXIT_FAILURE);
     }
+
     // Load xclbin
     std::cout << "Loading: '" << xclbinFilename << "'\n";
+
+    // Loading binary file to bin_file
     std::ifstream bin_file(xclbinFilename, std::ifstream::binary);
-    bin_file.seekg(0, bin_file.end);
-    unsigned nb = bin_file.tellg();
-    bin_file.seekg(0, bin_file.beg);
-    char* buf = new char[nb];
+
+    // Sets the position of the next character to be extracted from the input stream.
+    // istream& seekg (streamoff off, ios_base::seekdir way); off = offset from way - where you are offseting from
+    // Basically moving pointer in the stream
+    bin_file.seekg(0, bin_file.end); 
+    unsigned nb = bin_file.tellg();  // Returns the position of the current character in the input stream.
+    bin_file.seekg(0, bin_file.beg); // Setting position of the next character to be extracted from the input stream
+    char* buf = new char[nb]; // Creating buffer/array with the length of the stream => bin_file
+
+    // buf - pointer of the array where the extracted characters are stored
+    // nb – number of characters to extract.
     bin_file.read(buf, nb);
 
     // Creating Program from Binary File
     cl::Program::Binaries bins;
+    // {} initialization of vector - array
+    // push_back - Adds a new element at the end of the vector, after its current last element.
     bins.push_back({buf, nb});
-    bool valid_device = false;
-    for (unsigned int i = 0; i < devices.size(); i++) {
-        auto device = devices[i];
-        // Creating Context and Command Queue for selected Device
-        OCL_CHECK(err, context = cl::Context(device, nullptr, nullptr, nullptr, &err));
-        OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
-        std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
-        cl::Program program(context, {device}, bins, nullptr, &err);
-        if (err != CL_SUCCESS) {
-            std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
-        } else {
-            std::cout << "Device[" << i << "]: program successful!\n";
-            OCL_CHECK(err, krnl_vector_add = cl::Kernel(program, "krnl_vadd", &err));
-            valid_device = true;
-            break; // we break because we found a valid device
-        }
-    }
-    if (!valid_device) {
-        std::cout << "Failed to program any device found, exit!\n";
-        exit(EXIT_FAILURE);
-    }
+
+    // Resizes devices vector to contain just one device, which is used
+    devices.resize(1);
+    // OCL_CHECK wraps andn checks if function completed correctly
+
+    // cl:Program - Construct a program object from a list of devices and a per-device list of binaries.
+    // https://github.khronos.org/OpenCL-CLHPP/classcl_1_1_program.html#af804a4cb0e2dc844c0be0b0b76dbd25a
+    OCL_CHECK(err, program = cl::Program(context, devices, bins, NULL, &err));
+
+    // This call will get the kernel object from program. A kernel is an
+    // OpenCL function that is executed on the FPGA.
+    // https://github.khronos.org/OpenCL-CLHPP/classcl_1_1_kernel.html#a351422a7e7125812c23610628b0f9de7
+    OCL_CHECK(err, krnl_vector_add = cl::Kernel(program, "krnl_vadd", &err));
 
     // These commands will allocate memory on the Device. The cl::Buffer objects can
     // be used to reference the memory locations on the device.
+    // Allowed flags for cl::Buffer https://man.opencl.org/clCreateBuffer.html
+    // size_in_bytes is DATA_SIZE*sizeof(int) here 4096*
     OCL_CHECK(err, cl::Buffer buffer_a(context, CL_MEM_READ_ONLY, size_in_bytes, NULL, &err));
     OCL_CHECK(err, cl::Buffer buffer_b(context, CL_MEM_READ_ONLY, size_in_bytes, NULL, &err));
     OCL_CHECK(err, cl::Buffer buffer_result(context, CL_MEM_WRITE_ONLY, size_in_bytes, NULL, &err));
 
     // set the kernel Arguments
     int narg = 0;
+    // cl::Kernel::setArg(cl uint index, T value)
+    // Set the kernel argument; you do not need to specify the type as it is worked out from T using templates.
     OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, buffer_a));
     OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, buffer_b));
     OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, buffer_result));
     OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, DATA_SIZE));
 
     // We then need to map our OpenCL buffers to get the pointers
-    float* ptr_a;
-    float* ptr_b;
-    float* ptr_result;
+    int* ptr_a;
+    int* ptr_b;
+    int* ptr_result;
 
-    OCL_CHECK(err,
-              ptr_a = (float*)q.enqueueMapBuffer(buffer_a, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes, NULL, NULL, &err));
-    OCL_CHECK(err,
-              ptr_b = (float*)q.enqueueMapBuffer(buffer_b, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes, NULL, NULL, &err));
-    OCL_CHECK(err, ptr_result = (float*)q.enqueueMapBuffer(buffer_result, CL_TRUE, CL_MAP_READ, 0, size_in_bytes, NULL,
-                                                         NULL, &err));
-
-    ptr_a[0] = 2.0; // x0
-    ptr_b[0] = 12.5; // y0
+    // Enqueues a command to map a region of the buffer object given by buffer into the host address space and returns a pointer to this mapped regio
+    // https://registry.khronos.org/OpenCL/sdk/1.0/docs/man/xhtml/clEnqueueMapBuffer.html
+    OCL_CHECK(err, ptr_a = (int*)q.enqueueMapBuffer(buffer_a, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes, NULL, NULL, &err));
+    OCL_CHECK(err, ptr_b = (int*)q.enqueueMapBuffer(buffer_b, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes, NULL, NULL, &err));
+    OCL_CHECK(err, ptr_result = (int*)q.enqueueMapBuffer(buffer_result, CL_TRUE, CL_MAP_READ, 0, size_in_bytes, NULL, NULL, &err));
 
     // Data will be migrated to kernel space
     OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_a, buffer_b}, 0 /* 0 means from host*/));
 
     // Launch the Kernel
+    // Enqueues a command to execute a kernel on a device.
+    // https://registry.khronos.org/OpenCL/sdk/1.0/docs/man/xhtml/clEnqueueTask.html
+    // https://kosobucki.pl/cl_doc/classcl_1_1CommandQueue.html#a19f0b52a87a8a33d1280ef9e8f5149f3 with example
+    // original https://github.khronos.org/OpenCL-CLHPP/classcl_1_1_command_queue.html#a287d13ca6eb15f904419871dfc440ca6
     OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add));
 
     // The result of the previous kernel execution will need to be retrieved in
@@ -149,16 +192,29 @@ int main(int argc, char* argv[]) {
     // source_results vector
     OCL_CHECK(err, q.enqueueMigrateMemObjects({buffer_result}, CL_MIGRATE_MEM_OBJECT_HOST));
 
+
+    // Blocks until all previously queued OpenCL commands in a command-queue are issued to the associated device and have completed.
     OCL_CHECK(err, q.finish());
 
+    // Verify the result
+    // Calculate the same thing on CPU
+   
+    for (int i = 0; i < DATA_SIZE; i++) {
+        int host_result = ptr_a[i] + ptr_b[i];
+        if (ptr_result[i] != host_result) {
+            printf(error_message.c_str(), i, host_result, ptr_result[i]);
+            match = 1;
+            break;
+        }
+    }
 
-
-
+    // Enqueues a command to unmap a previously mapped region of a memory object. 
     OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_a, ptr_a));
     OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_b, ptr_b));
     OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_result, ptr_result));
+    // Blocks until all previously queued OpenCL commands in a command-queue are issued to the associated device and have completed.
     OCL_CHECK(err, err = q.finish());
 
-    std::cout << "Result from kernel:\n"<<ptr_result[0]<<"\n"<<std::endl;
-    return 0;
+
+    return (match ? EXIT_FAILURE : EXIT_SUCCESS);
 }
