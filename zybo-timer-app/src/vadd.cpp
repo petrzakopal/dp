5bed49a30a4bb117c3e83b376918d5c00bd3529c
petrzakopal <petr.zakopal@gmail.com>
2023-03-11 01:31:02 +0100

    refactored with functions, slower

--- zybo-timer-app/src/vadd.cpp
+++ zybo-timer-app/src/vadd.cpp
@@ -78,11 +78,155 @@ ALL TIMES.
 
 static const int DATA_SIZE = 4096;
 
-static const std::string error_message =
-    "Error: Result mismatch:\n"
-    "i = %d CPU result = %d Device result = %d\n";
 
 int main(int argc, char* argv[]) {
+
+    /*-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-*/
+    /*-------------------------------- SETTING THE ENVIRONMENT ---------------------------------*/
+    // TARGET_DEVICE macro needs to be passed from gcc command line
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <xclbin>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string xclbinFilename = argv[1];
+
+    /*-------------------------------------------------------------------------------------*/
+    /*-------------------------------- OPENCL DEFINITIONS ---------------------------------*/
+    std::vector<cl::Device> devices;
+    cl_int err;
+    cl::Context context;
+    cl::CommandQueue q;
+    cl::Program program;
+    std::vector<cl::Platform> platforms;
+    bool found_device = false;
+    /*-----------------------------------------------------------------------------------------*/
+
+    /*-------------------------------- OPENCL KERNEL CREATION ---------------------------------*/
+    cl::Kernel krnl_main;
+    // cl::Kernel krnl_GenerateVoltageAlphaBeta;
+    /*----------------------------------------------------------------------------------------*/
+
+   
+    /*-------------------------------------------------------------------------------------*/
+    /*------------------------- PLATFORM SEARCHING AND SELECTING --------------------------*/
+
+    // Searching through devices on a platform and connecting from CPU to device (PL)
+    cl::Platform::get(&platforms);
+
+    // Loop through all the platforms based on a vector of platforms size till the device is found (found_device==true)
+    for (size_t i = 0; (i < platforms.size()) & (found_device == false); i++)
+    {
+        cl::Platform platform = platforms[i];
+        std::string platformName = platform.getInfo<CL_PLATFORM_NAME>();
+
+        // If the found device is Xilinx device
+        if (platformName == "Xilinx")
+        {
+            devices.clear();
+            // Gets list of devices for selected platform and pastes them on the adress of devices Device
+            platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices);
+            if (devices.size())
+            {
+                found_device = true;
+                break;
+            }
+        }
+    }
+
+    if (found_device == false) {
+        std::cout << "Error: Unable to find Target Device " << std::endl;
+        return EXIT_FAILURE;
+    }
+    /*-------------------------------------------------------------------------------------*/
+
+
+    /*-------------------------------------------------------------------------------------*/
+    /*--------------------- LOADING AND READING XCLBIN FILE TO FPGA ----------------------*/
+    // Check for availability of PL program
+    std::cout << "INFO: Reading " << xclbinFilename << std::endl;
+    FILE* fp;
+    if ((fp = fopen(xclbinFilename.c_str(), "r")) == nullptr)
+    {
+        printf("ERROR: %s xclbin not available please build\n", xclbinFilename.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    // Load xclbin
+    std::cout << "Loading: '" << xclbinFilename << "'\n";
+    std::ifstream bin_file(xclbinFilename, std::ifstream::binary);
+
+    // Sets the position of the next character to be extracted from the input stream.
+    // istream& seekg (streamoff off, ios_base::seekdir way); off = offset from way - where you are offseting from
+    // Basically moving pointer in the stream
+    bin_file.seekg(0, bin_file.end); 
+    unsigned nb = bin_file.tellg();  // Returns the position of the current character in the input stream.
+    bin_file.seekg(0, bin_file.beg); // Setting position of the next character to be extracted from the input stream
+    char* buf = new char[nb]; // Creating buffer/array with the length of the stream => bin_file
+
+    // buf - pointer of the array where the extracted characters are stored
+    // nb – number of characters to extract.
+    bin_file.read(buf, nb);
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /*-------------------------------------------------------------------------------------*/
+    /*------------------------------- PROGRAMMING THE FPGA -------------------------------*/
+    // Creating Program from Binary File
+    cl::Program::Binaries bins;
+    // {} initialization of vector - array
+    // push_back - Adds a new element at the end of the vector, after its current last element.
+    bins.push_back({buf, nb});
+    bool valid_device = false;
+
+    
+    for (unsigned int i = 0; i < devices.size(); i++)
+    {
+        auto device = devices[i];
+        // Creating Context and Command Queue for selected Device
+
+        OCL_CHECK(err, context = cl::Context(device, nullptr, nullptr, nullptr, &err));
+
+        OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+
+        std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+
+        // cl:Program - Construct a program object from a list of devices and a per-device list of binaries.
+        cl::Program program(context, {device}, bins, nullptr, &err);
+
+        if (err != CL_SUCCESS)
+        {
+            std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
+        }
+        else
+        {
+            std::cout << "Device[" << i << "]: program successful!\n";
+
+
+            /*-------------------------------- SELECTING KERNEL FROM KERNEL FILE ---------------------------------*/
+
+            // This call will get the kernel object from program. A kernel is an
+            // OpenCL function that is executed on the FPGA.
+            OCL_CHECK(err, krnl_main = cl::Kernel(program, "krnl_vadd", &err)); // assign to krnl_compute the new PL kernel, which is found in kernel_main and called kernel_main
+
+            valid_device = true;
+
+           
+
+            break; // break because found a valid device
+
+            /*------------------------------------------------------------------------------------------------------*/
+        }
+    }
+    if (!valid_device) {
+        std::cout << "Failed to program any device found, exit!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    /*-------------------------------- END OF SETTING THE ENVIRONMENT ---------------------------------*/
+    /*-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-*/
+
 
 
     std::cout << "interrupt testing part\n";
@@ -120,63 +264,93 @@ int main(int argc, char* argv[]) {
     std::cout << "sizeof(ptr)= " << sizeof(ptr) << "\n";
     std::cout << "sizeof(fd)= " << sizeof(fd) << "\n";
     
+    // unsigned data = 0xC0;
+   
+ //first type kernel
+    OCL_CHECK(err, cl::Buffer buffer_memory(context, CL_MEM_USE_HOST_PTR, sizeof((unsigned *) ptr),ptr,&err));
+   
+
+    int narg = 0;
+    OCL_CHECK(err, err = krnl_main.setArg(narg++, buffer_memory));
+    
+
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_memory}, 0 /* 0 means from host*/));
+    
+    OCL_CHECK(err, err = q.enqueueTask(krnl_main));
+    OCL_CHECK(err, q.enqueueMigrateMemObjects({buffer_memory}, CL_MIGRATE_MEM_OBJECT_HOST));
+
+    // OCL_CHECK(err, q.finish());
+    printf("Kernel finished, check value.\n");
+    value = *((unsigned *) (ptr));
+    printf("timer counter register: %08x\n", value);
+    std::cout << "addres prt: " << ptr << "\n";
     /* Enable All Interrupts */
     printf("Enable Interrupt and Start for the first time!\n");
-    *((unsigned *)(ptr)) = 0X0;
-    
-    
-    uint32_t info = 1;
+
+    printf("Second run of a kernel!\n");
    
+    
+
+    for(int i = 0;i<500;i++)
+    {
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_memory}, 0 /* 0 means from host*/));
+    OCL_CHECK(err, err = q.enqueueTask(krnl_main));
+    
+    OCL_CHECK(err, q.enqueueMigrateMemObjects({buffer_memory}, CL_MIGRATE_MEM_OBJECT_HOST));
+    
+    }
+    OCL_CHECK(err, q.finish());
+
      
-     *((unsigned *)(ptr)) = 0XC0;
+    //  *((unsigned *)(ptr)) = 0XC0;
   
 
-    printf("before reading\n");
-    /* Wait for interrupt */
+    // printf("before reading\n");
+    // /* Wait for interrupt */
       
-        printf("right after reading\n");
-        while(1)
-        {
+    //     printf("right after reading\n");
+    //     while(1)
+    //     {
 
 
-        // when used non blocking read O_NONBLOCK
-        // ssize_t nb = read(fd, &info, sizeof(info));
-        // value = *((unsigned *) (ptr + TIMER_0_CTR_REG));
-        // printf("timer counter register: %08x\n", value);
-        // if (nb == (ssize_t)sizeof(info))
-        // {
-        //     /* Do something in response to the interrupt. */
-        //     printf("Interrupt #%u!\n", info);
-        //     break;
-        // } 
+    //     // when used non blocking read O_NONBLOCK
+    //     // ssize_t nb = read(fd, &info, sizeof(info));
+    //     // value = *((unsigned *) (ptr + TIMER_0_CTR_REG));
+    //     // printf("timer counter register: %08x\n", value);
+    //     // if (nb == (ssize_t)sizeof(info))
+    //     // {
+    //     //     /* Do something in response to the interrupt. */
+    //     //     printf("Interrupt #%u!\n", info);
+    //     //     break;
+    //     // } 
 
         
-         int ret = poll(&fds, 1, -1);
+    //      int ret = poll(&fds, 1, -1);
 
-         value = *((unsigned *) (ptr + TIMER_0_CTR_REG));
-            printf("timer counter register: %08x\n", value);
-        if (ret >= 1) {
-            ssize_t nb = read(fd, &info, sizeof(info));
+    //      value = *((unsigned *) (ptr + TIMER_0_CTR_REG));
+    //         printf("timer counter register: %08x\n", value);
+    //     if (ret >= 1) {
+    //         ssize_t nb = read(fd, &info, sizeof(info));
             
 
-            if (nb == (ssize_t)sizeof(info)) {
-                /* Do something in response to the interrupt. */
-               printf("Interrupt #%u!\n", info);
-            break;
-            }
+    //         if (nb == (ssize_t)sizeof(info)) {
+    //             /* Do something in response to the interrupt. */
+    //            printf("Interrupt #%u!\n", info);
+    //         break;
+    //         }
 
 
-        }
+    //     }
 
-        }
+    //     }
        
        
-    *((unsigned *)(ptr)) = 0X1C0;
-     write(fd, &irq_on, sizeof(irq_on));
-    *((unsigned *)(ptr + 0x4)) = 0XAFFFFFFF;
-    *((unsigned *)(ptr)) = 0XE0;
+    // *((unsigned *)(ptr)) = 0X1C0;
+    //  write(fd, &irq_on, sizeof(irq_on));
+    // *((unsigned *)(ptr + 0x4)) = 0XAFFFFFFF;
+    // *((unsigned *)(ptr)) = 0XE0;
   
-    *((unsigned *)(ptr + 0x8)) = 0X0;
+    // *((unsigned *)(ptr + 0x8)) = 0X0;
     munmap(ptr, TIMER_MAP_SIZE);
     close(fd);
     return (0);
--- zybo-timer-app_kernels/src/krnl_vadd.cpp
+++ zybo-timer-app_kernels/src/krnl_vadd.cpp
@@ -91,76 +91,21 @@ Input Vector 2 from Global Memory --->|             |      |__|
 #include <fstream>
 #include <iostream>
 
-#define DATA_SIZE 4096
 #define IN 0
 #define OUT 1
 
 #define TIMER_MAP_SIZE 0x10000
 #define TIMER_0_CTR_REG 0x08
 
-// TRIPCOUNT identifier
-const int c_size = DATA_SIZE;
-
-static void load_input(uint32_t* in, hls::stream<uint32_t>& inStream, int size) {
-mem_rd:
-    for (int i = 0; i < size; i++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-        inStream << in[i];
-    }
-}
-
-static void compute_add(hls::stream<uint32_t>& in1_stream,
-                        hls::stream<uint32_t>& in2_stream,
-                        hls::stream<uint32_t>& out_stream,
-                        int size) {
-// The kernel is operating with vector of NUM_WORDS integers. The + operator performs
-// an element-wise add, resulting in NUM_WORDS parallel additions.
-execute:
-    for (int i = 0; i < size; i++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-        out_stream << (in1_stream.read() + in2_stream.read());
-    }
-}
-
-static void store_result(uint32_t* out, hls::stream<uint32_t>& out_stream, int size) {
-mem_wr:
-    for (int i = 0; i < size; i++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-        out[i] = out_stream.read();
-    }
-}
 
 extern "C" {
 
-/*
-    Vector Addition Kernel
 
-    Arguments:
-        in1  (input)  --> Input vector 1
-        in2  (input)  --> Input vector 2
-        out  (output) --> Output vector
-        size (input)  --> Number of elements in vector
-*/
-
-void krnl_vadd(uint32_t* in1, uint32_t* in2, uint32_t* out, int size, void *ptr) {
-#pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
-#pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
-#pragma HLS INTERFACE m_axi port = out bundle = gmem0
-
-    static hls::stream<uint32_t> in1_stream("input_stream_1");
-    static hls::stream<uint32_t> in2_stream("input_stream_2");
-    static hls::stream<uint32_t> out_stream("output_stream");
+void krnl_vadd(void *ptr) {
 
 
-
-
-#pragma HLS dataflow
-    // dataflow pragma instruct compiler to run following three APIs in parallel
-    load_input(in1, in1_stream, size);
-    load_input(in2, in2_stream, size);
-    compute_add(in1_stream, in2_stream, out_stream, size);
-    store_result(out, out_stream, size);
-
+  
+  *((unsigned *)(ptr)) = 0XC0;
 
 }
 }
