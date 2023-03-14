c8c0c83affe4117ecdb90ec9a18090905e191b85
petrzakopal <petr.zakopal@gmail.com>
2023-03-14 13:22:15 +0100

    added sigterm to testing file

--- zybo-register/src/vadd.cpp
+++ zybo-register/src/vadd.cpp
@@ -1,46 +1,4 @@
-/*******************************************************************************
-Vendor: Xilinx
-Associated Filename: vadd.cpp
-Purpose: VITIS vector addition
 
-*******************************************************************************
-Copyright (C) 2019 XILINX, Inc.
-
-This file contains confidential and proprietary information of Xilinx, Inc. and
-is protected under U.S. and international copyright and other intellectual
-property laws.
-
-DISCLAIMER
-This disclaimer is not a license and does not grant any rights to the materials
-distributed herewith. Except as otherwise provided in a valid license issued to
-you by Xilinx, and to the maximum extent permitted by applicable law:
-(1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX
-HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
-INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
-FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
-in contract or tort, including negligence, or under any other theory of
-liability) for any loss or damage of any kind or nature related to, arising under
-or in connection with these materials, including for any direct, or any indirect,
-special, incidental, or consequential loss or damage (including loss of data,
-profits, goodwill, or any type of loss or damage suffered as a result of any
-action brought by a third party) even if such damage or loss was reasonably
-foreseeable or Xilinx had been advised of the possibility of the same.
-
-CRITICAL APPLICATIONS
-Xilinx products are not designed or intended to be fail-safe, or for use in any
-application requiring fail-safe performance, such as life-support or safety
-devices or systems, Class III medical devices, nuclear facilities, applications
-related to the deployment of airbags, or any other applications that could lead
-to death, personal injury, or severe property or environmental damage
-(individually and collectively, "Critical Applications"). Customer assumes the
-sole risk and liability of any use of Xilinx products in Critical Applications,
-subject only to applicable laws and regulations governing limitations on product
-liability.
-
-THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
-ALL TIMES.
-
-*******************************************************************************/
 
 #define OCL_CHECK(error, call)                                                                   \
     call;                                                                                        \
@@ -63,6 +21,8 @@ ALL TIMES.
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <poll.h>
+#include <cstdlib>
+#include <signal.h>
 
 static const int DATA_SIZE = 4096;
 
@@ -70,7 +30,22 @@ static const std::string error_message =
     "Error: Result mismatch:\n"
     "i = %d CPU result = %d Device result = %d\n";
 
+
+// Define the function to be called when ctrl-c (SIGINT) is sent to process
+// for setting regulators to 0 and voltages to 0 when user interrupts the program
+void signal_callback_handler(int signum) {
+   std::cout << "\nCaught signal " << signum << "\n";
+   std::cout << "Interrupted by user.\n";
+   // Terminate program
+   exit(signum);
+}
+
+
 int main(int argc, char* argv[]) {
+
+ // Register signal and signal handler
+   signal(SIGINT, signal_callback_handler);
+
     // TARGET_DEVICE macro needs to be passed from gcc command line
     if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <xclbin>" << std::endl;
@@ -257,6 +232,8 @@ int main(int argc, char* argv[]) {
     OCL_CHECK(err, q.enqueueMigrateMemObjects({buffer_register}, CL_MIGRATE_MEM_OBJECT_HOST));
     OCL_CHECK(err, q.finish());
      std::cout << "data: " << data_val << "\n";
+
+   
 
     return (0);
 }
